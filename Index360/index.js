eeb3d8cada510b6566e9372317e32b9f57117df9
prashanth-g <prashanthcsgce@gmail.com>
2018-12-09 13:08:53 +0530

    Initial Commit

---
+++ Index360/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  asset, 
+  NativeModules
+} from 'react-360';
+
+const {AudioModule} = NativeModules;
+
+AudioModule.playEnvironmental({
+  source: asset('path/to/audio.mp3'),
+  volume: 0.3, // play at 3/10 original volume
+});
+
+export default class Index360 extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>
+            Welcome to React 360
+          </Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+  greeting: {
+    fontSize: 30,
+  },
+});
+
+AppRegistry.registerComponent('Index360', () => Index360);
